647d0e4752c45317e20b33c2f20d68392a1ffbeb
lizhuangzhuangzhuang <31616138+lizhuangzhuangzhuang@users.noreply.github.com>
2024-01-31 17:40:35 +0800

    feat(model):implement data model and data model converter function.

--- src/main/java/com/mangosteen/app/controller/HelloController.java
+++ src/main/java/com/mangosteen/app/controller/HelloController.java
@@ -1,6 +1,8 @@
 package com.mangosteen.app.controller;
 
+import com.mangosteen.app.model.dao.UserInfo;
 import org.springframework.web.bind.annotation.*;
+import lombok.val;
 
 @RestController
 public class HelloController {
@@ -20,6 +22,8 @@ public class HelloController {
     public String sayGreeting (@RequestParam("name") String name,
                             @RequestParam("id") long id
     ){
+        val userInfo = UserInfo.builder().username("xxxx").build();
+        System.out.println(userInfo.getId());
         return String.format("Hello  %s with %d", name, id);
     }
 }
